c6c61f73aa531a67ff2b05424807da380483befe
Sumnut <mattoneill66@yahoo.ca>
2016-02-12 04:57:37 -0500

    Testing Script updated, Log folder added, need better log file naming scheme

--- Assignment_1_Testing/testing.sh
+++ Assignment_1_Testing/testing.sh
@@ -2,38 +2,60 @@
 program="$1"
 outputfile="$2"
 tfolder="$3"
+
+if [ "$4" == "" ]; then
+	transFolder=""
+else
+	transFolder="$4"
+fi
+start=$(date +%s%N)
+
+date=$(date +"%m-%d-%y-%T")
 echo Start
-echo $tfolder > log.txt
-for folder_name in $(ls $tfolder);
+echo "" > log.txt
+for folder_name in $(ls $tfolder | grep "$transFolder");
 do
 	folder=$tfolder"/"$folder_name
 	for tests in $(ls $folder)
 	do
 		testfolder=$folder"/"$tests
 		./$program $testfolder"/test.txt" > resultt.txt
+		
+		status=""
+		echo $tests "Testing: Started" >> log.txt
+		
 		filediff=$(diff $outputfile $testfolder"/result.txt");
-		filedifff=$(diff resultt.txt $testfolder"/commandlineResult.txt");
-
 		if [ "$filediff" == "" ]; then
-			if [ "$filedifff" == "" ]; then
-				echo $(tput setaf 2)"  "$folder_name": "$tests "Test - Success" 
-			else
-				echo $(tput setaf 1)"  "$folder_name": "$tests "Test - Failure"
-				echo $tests "Testing: Started" >> log.txt
-				echo $tests "result.txt: Success" >> log.txt
-				echo $tests "Cresult.txt: Failure" >> log.txt
-				echo $filedifff >> log.txt
-				echo $tests "Testing: Complete" >> log.txt
-			fi
+			echo -e "\t"$tests "Result: Success" >> log.txt
 		else
-			echo $(tput setaf 1)"  "$folder_name": "$tests "Test - Failure"
-			echo $tests "Testing: Started" >> log.txt
-			echo $tests "result.txt: Failure" >> log.txt
-			echo $tests "Cresult.txt: Unknown" >> log.txt
-			echo $filediff >> log.txt
-			echo $filedifff >> log.txt
-			echo $tests "Testing: Complete" >> log.txt
+			echo -e "\t"$tests "Result: Failure" >> log.txt
+			echo -e "\t"$filediff >> log.txt
+			status="Fail"
+		fi
+
+		filediff=$(diff resultt.txt $testfolder"/commandlineResult.txt");
+		if [ "$filediff" == "" ]; then
+			echo -e "\t"$tests "Command Line Result: Success" >> log.txt
+		else
+			echo -e "\t"$tests "Command Line Result: Failure" >> log.txt
+			echo -e "\t"$filediff >> log.txt
+			status="Fail"
+		fi
+		echo $tests "Testing: Complete" >> log.txt
+		echo "" >> log.txt
+
+		if [ "$status" == "" ]; then
+			echo $(tput setaf 2)"  "$folder_name": "$tests "Test - Success" 
+		else
+			echo $(tput setaf 1)"  "$folder_name": "$tests "Test - Failure" 
 		fi
 	done
 done
-echo $(tput setaf 7)Done
+
+end=$(date +%s%N)
+result=$(((end-start)/1000000))
+echo $(tput setaf 7)"Time to Complete: " $result "ms"
+
+echo "Time to Complete: " $result "ms" >> log.txt
+mv log.txt Logs/"log_"$date".log"
+echo Done
